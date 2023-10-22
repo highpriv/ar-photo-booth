7f6a0378db48d46173366e34a7b96a85d62fdc57
Canberk <highpriv@gmail.com>
2023-10-22 22:32:34 +0300

    first flow sklt

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,21 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import styles from "@/styles/Home.module.css";
+import NBALogo from "@/assets/nba.svg";
 
 export default function Home() {
   return (
-    <div>
-      <h2>Hello!</h2>
+    <div className={styles.container}>
+      <div className="flex flex-col items-center justify-center w-full h-full">
+        <div className="flex flex-col h-full w-full items-center justify-center">
+          <h1 className="uppercase text-center">
+            TAKE A PHOTO WITH YOUR FAVORITE NBA PLAYER
+          </h1>
+          <button className="btn btn-primary uppercase">Tap to Begin</button>
+        </div>
+        <div className="">
+          <Image src={NBALogo} alt="NBA Logo" height={200} />
+        </div>
+      </div>
     </div>
   );
 }
