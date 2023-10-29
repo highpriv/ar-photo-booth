ae09dcb51349d689a751bedaad1c556001b0053a
Canberk <highpriv@gmail.com>
2023-10-29 23:53:03 +0300

    update

--- pages/step-3/index.js
+++ pages/step-3/index.js
@@ -1,14 +1,22 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import Webcam from "react-webcam";
 
 export default function Step_2() {
+  const webcamRef = useRef(null);
   const [next, setNext] = useState(false);
   const [counter, setCounter] = useState(10);
   const [isPhotoTaken, setIsPhotoTaken] = useState(false);
+  const [userImage, setUserImage] = useState(null);
+  const [imagePosition, setImagePosition] = useState({ x: 0, y: 0 });
 
   setTimeout(() => {
     if (next) {
       if (counter > 0) {
         setCounter(counter - 1);
+      }
+
+      if (counter === 0) {
+        capture();
       }
     }
   }, 1000);
@@ -19,8 +27,64 @@ export default function Step_2() {
     }
   }, [counter]);
 
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+    const imageUrl = URL.createObjectURL(file);
+    setUserImage(imageUrl);
+  };
+
+  const capture = () => {
+    debugger;
+    const imageSrc = webcamRef.current.getScreenshot();
+    if (userImage) {
+      // Combine the camera image and user-selected image
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d");
+      const webcamImg = new Image();
+      const userImg = new Image();
+
+      webcamImg.src = imageSrc;
+      userImg.src = userImage;
+
+      webcamImg.onload = () => {
+        canvas.width = webcamImg.width;
+        canvas.height = webcamImg.height;
+        ctx.drawImage(webcamImg, 0, 0);
+        if (imagePosition) {
+          ctx.drawImage(userImg, imagePosition.x, imagePosition.y);
+        }
+        const finalImage = canvas.toDataURL("image/png");
+        saveAs(finalImage, "edited-image.png");
+      };
+    } else {
+      // Handle the case when the user hasn't uploaded an image
+    }
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setNext(true);
+    }, 3000);
+  }, []);
+
   return (
     <div className="text-white flex justify-center h-screen">
+      <>
+        <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
+        {userImage && (
+          <>
+            <img
+              src={userImage}
+              alt="User's Image"
+              style={{
+                left: imagePosition.x + "px",
+                top: imagePosition.y + "px",
+              }}
+            />
+            sa - {JSON.stringify(imagePosition)}
+          </>
+        )}
+      </>
       {!isPhotoTaken ? (
         <div
           className={`flex flex-col justify-center items-center shadow-2xl bg-white w-1/3 portrait:w-full`}
@@ -28,10 +92,7 @@ export default function Step_2() {
           <div className="px-5">
             <div className="flex flex-col items-center gap-10">
               {!next ? (
-                <h1
-                  onClick={() => setNext(true)}
-                  className="uppercase text-center text-6xl portrait:text-9xl font-bold [text-shadow:_0px_2px_0px_rgb(129_159_196_/_100%)]"
-                >
+                <h1 className="uppercase text-center text-6xl portrait:text-9xl font-bold [text-shadow:_0px_2px_0px_rgb(129_159_196_/_100%)]">
                   Get into position!
                 </h1>
               ) : (
